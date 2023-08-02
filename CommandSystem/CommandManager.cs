984d854e853588d5bd147af29827549694350828
Shadow System <ShadowSystemSS@outlook.com>
2023-08-02 19:37:53 +0300

    CommandManager replace Intercept on Execute, move Execute in Command

--- CommandSystem/CommandManager.cs
+++ CommandSystem/CommandManager.cs
@@ -1,21 +1,17 @@
 ﻿namespace CommandSystem;
 
-public abstract class CommandManager : CommandContainer, ITextCommand
+public abstract class CommandManager : CommandContainer
 {
     protected abstract void Message(string? text);
-    protected abstract bool Intercept(string? text);
 
-    public ICommand? Execute(string? text)
+    public void Command(string? text)
     {
-        if (Intercept(text) && _step is ITextCommand textCommand)
+        if (_step is ITextCommand textCommand)
             _step = textCommand.Execute(text);
 
         if (_step is IMessageCommand messageCommand)
             Message(messageCommand.Message);
 
         _step = _step?.Execute();
-        return this;
     }
-
-    public abstract ICommand? Execute();
 }
